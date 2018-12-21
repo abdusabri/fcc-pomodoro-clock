7a2008a229e72d2a63e110b7b25d2dfd871b8f5d
Abdu Assabri <abdusabri@outlook.com>
2018-12-21 15:37:01 +0100

    Updates

---
+++ precache-manifest.ea48db6a2e00509965fc732603e2d3b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ad5dc8ced7a11fe8830",
+    "url": "/fcc-pomodoro-clock/static/js/runtime~main.1ad5dc8c.js"
+  },
+  {
+    "revision": "d38a70bdf46ac20641fb",
+    "url": "/fcc-pomodoro-clock/static/js/main.d38a70bd.chunk.js"
+  },
+  {
+    "revision": "4b97957d184e500996fa",
+    "url": "/fcc-pomodoro-clock/static/js/1.4b97957d.chunk.js"
+  },
+  {
+    "revision": "d38a70bdf46ac20641fb",
+    "url": "/fcc-pomodoro-clock/static/css/main.2bef4a33.chunk.css"
+  },
+  {
+    "revision": "a5c2e872b41818c4714d33a8889be2aa",
+    "url": "/fcc-pomodoro-clock/index.html"
+  }
+];
